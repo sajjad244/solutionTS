a14b573a4b9981902b8acc7210fba2a9b7ef7a6d
sajjad244 <mrsajjed@gmail.com>
2025-11-17 09:09:43 +0600

    solution 1 done

--- solution.ts
+++ solution.ts
@@ -1,1 +1,9 @@
- 
+function formatValue(value: string | number | boolean): unknown {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  } else if (typeof value === "number") {
+    return value * 10;
+  } else if (typeof value === "boolean") {
+    return !value;
+  }
+}
