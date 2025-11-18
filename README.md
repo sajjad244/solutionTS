91a65cda21572e9d208331ad7717a82e3d4dded7
sajjad244 <mrsajjed@gmail.com>
2025-11-18 12:54:39 +0600

    complete task

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
+Questions link : https://github.com/Apollo-Level2-Web-Dev/B6A1
 
+1.  What are some differences between interfaces and types in TypeScript?
 
+TypeScript-এ interface আর type—দুটোই ডেটার গঠন করতে ব্যবহার হয়। কিন্তু কিছু জায়গায় এদের আচরণ আলাদা।
+
+====> interface ⇒ object/structure/class
+
+# Interface একাধিকবার একই নামে ঘোষণা করা যায়। TypeScript এগুলোকে একত্রে মিলিয়ে নেয়। মানে interface একটু "ফ্লেক্সিবল"।
+
+# Interface সাধারণত object-এর গঠন বোঝাতে ব্যবহার হয় এবং ক্লাসের সঙ্গে বেশি ব্যবহার করা হয়।
+
+# অবজেক্টের structure বা ক্লাসের contract ঠিক করতে interface.
+
+====> Type ⇒ বেশি ফ্লেক্সিবল, জটিল টাইপ বানানোর জন্য ভালো
+
+# Type একই নামে দুবার ঘোষণা করা যায় না। করলে error দেয়।মানে type একটু "স্ট্রিক্ট"।
+
+# Type দিয়ে বেশি ধরনের কাজ করা যায়। যেমন: union, intersection, tuple ইত্যাদি। শুধু object না—আরো জটিল টাইপও বানানো যায়।
+
+# জটিল টাইপ বা মিশ্র ডেটা দরকার হলে type.
+
+2.  Provide an example of using union and intersection types in TypeScript.
+
+TypeScript-এ union টাইপ তখন ব্যবহার করা হয় যখন একটা ভেরিয়েবল একাধিক ধরনের ডেটা নিতে পারে। যেমন একটা আইডি কখনও নম্বর হবে, কখনও স্ট্রিং হবে। সেক্ষেত্রে টাইপ হবে number | string। এতে কোড আরও flexible হয়।
+
+আর intersection টাইপ ঠিক উল্টো—এটা দুই বা তার বেশি টাইপকে একসাথে জোড়া লাগিয়ে নতুন টাইপ বানায়। যেমন আমার কাছে User আর Address নামে দুইটা টাইপ আছে। আমি চাইলে এগুলোকে & দিয়ে একত্রে করে UserWithAddress বানাতে পারি । এতে দুইটাই একসাথে থাকবে।
+
+union = এটা অথবা ওটা,
+intersection = দুইটাই একসাথে।
--- solution.ts
+++ solution.ts
@@ -1,9 +1,125 @@
-function formatValue(value: string | number | boolean): unknown {
+function formatValue(
+  value: string | number | boolean
+): string | number | boolean {
   if (typeof value === "string") {
     return value.toUpperCase();
   } else if (typeof value === "number") {
     return value * 10;
-  } else if (typeof value === "boolean") {
+  } else {
     return !value;
   }
+}
+
+function getLength(value: string | any[]): number {
+  if (typeof value === "string") {
+    return value.length;
+  } else if (Array.isArray(value)) {
+    return value.length;
+  } else {
+    return 0;
+  }
+}
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails(): string {
+    return `'Name: ${this.name}, Age: ${this.age}'`;
+  }
+}
+
+type Item = {
+  title: string;
+  rating: number;
+};
+
+function filterByRating(items: Item[]): Item[] {
+  return items.filter((item) => item.rating >= 4);
+}
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+};
+
+function filterActiveUsers(users: User[]): User[] {
+  return users.filter((user) => user.isActive === true);
+}
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+function printBookDetails(book: Book): void {
+  console.log(
+    `Title: ${book.title}, Author: ${book.author}, Published Year: ${
+      book.publishedYear
+    }, Available: ${book.isAvailable ? "Yes" : "No"}`
+  );
+}
+
+function getUniqueValues(
+  arr1: (number | string)[],
+  arr2: (number | string)[]
+): (number | string)[] {
+  const uniqueArray: (number | string)[] = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    let duplicateArr = false;
+    for (let j = 0; j < uniqueArray.length; j++) {
+      if (arr1[i] === uniqueArray[j]) {
+        duplicateArr = true;
+        break;
+      }
+    }
+    if (!duplicateArr) {
+      uniqueArray.push(arr1[i]);
+    }
+  }
+
+  for (let i = 0; i < arr2.length; i++) {
+    let duplicateArr = false;
+    for (let j = 0; j < uniqueArray.length; j++) {
+      if (arr2[i] === uniqueArray[j]) {
+        duplicateArr = true;
+        break;
+      }
+    }
+    if (!duplicateArr) {
+      uniqueArray.push(arr2[i]);
+    }
+  }
+
+  return uniqueArray;
+}
+
+type Product = {
+  name: string;
+  price: number;
+  quantity: number;
+  discount?: number;
+};
+
+function calculateTotalPrice(products: Product[]): number {
+  let totalPrice = 0;
+
+  products.map((product) => {
+    const normalPriceTotal = product.price * product.quantity;
+    const discountPrice = product.discount
+      ? (normalPriceTotal * product.discount) / 100
+      : 0;
+    totalPrice += normalPriceTotal - discountPrice;
+  });
+  return totalPrice;
 }
